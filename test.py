d079a647b8b866895faa332c29a2055e87975bfb
Prabhav Borate <66090042+prabhavborate92@users.noreply.github.com>
2023-02-05 18:59:32 -0500

    Add files via upload

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ myarr = np.random.randint(1000, size=100000)
 plt.hist(myarr, bins=40)
 plt.show()
 
-cd /home/pgb5080/output
+#cd /home/pgb5080/output
 sum.to_csv("sum.csv", index=False)
 myarr.to_csv("myarr.csv", index=False)
 
