65ac96a7833c163a2fadf402cf924857d8b694bc
Prabhav Borate <66090042+prabhavborate92@users.noreply.github.com>
2023-02-05 19:50:31 -0500

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,23 @@
+sum = 0
+for x in [1,2,3,4,5]:
+      sum = sum + x
+
+print(sum)
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt 
+myarr = np.random.randint(1000, size=100000)
+plt.hist(myarr, bins=40)
+plt.show()
+
+import os
+os.chdir(/home/pgb5080/output)
+
+f = open("sum.txt", "w")
+f.write(str(sum))
+f.close()
+
+f = open("myarr.txt", "w")
+f.write(str(myarr))
+f.close()
